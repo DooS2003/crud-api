4a214019105f7b5e61f15223772f4c1c5d5466d9
DooS2003 <Lepal123@hotmail.com>
2025-01-24 16:54:03 -0500

    Seeder

---
+++ database/seeders/departamentoSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class departamentoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('cargo')->insert([
+            ['codigo' => 'DEP001', 'nombre' => 'Recursos Humanos', 'activo' => true, 'idUsuarioCreacion' => 1],
+            ['codigo' => 'DEP002', 'nombre' => 'Finanzas', 'activo' => true, 'idUsuarioCreacion' => 1],
+            ['codigo' => 'DEP003', 'nombre' => 'IT', 'activo' => true, 'idUsuarioCreacion' => 1],
+        ]);
+    }
+}
